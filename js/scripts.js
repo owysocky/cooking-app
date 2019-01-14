b3b9cd30209754f54d7b40e5976cbd3b762ba3fd
Olha Wysocky and Pavel Zanchuk <pavel@>
2019-01-14 10:44:42 -0800

    initial set up

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter first nummber: "));
+var number2 = parseInt(prompt("Enter second number: "));
+
+alert(add(number1, number2));
