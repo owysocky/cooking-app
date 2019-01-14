1210a23a497c8a524f2f2a8626fa149d3cfd12fc
Olha Wysocky and Pavel Zanchuk <olha@>
2019-01-14 11:43:46 -0800

    initial setup

---
+++ README.md
@@ -0,0 +1,23 @@
+# _Cooking app_
+
+#### _Cooking application 01/11/2019_
+
+#### By _**Olha Wysocky and Pavel Zanchuk**_
+
+## Description
+
+_This is a cooking application to practice and learn how to use JS_
+
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _ATOM_
+* _JS_
+
+### License
+MIT
+**
+
+Copyright (c) 2019 **_Olha Wysocky and Pavel Zanchuk_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
-function add(number1, number2) {
-return number1 + number2;
-};
+function gallonToLiter(gallon){
+	return gallon * 3.785;
+}
+var gallon = parseInt(prompt("Please enter gallon(s): "))
+alert(gallon+" is "+gallonToLiter(gallon)+ "liters.");
 
-var number1 = parseInt(prompt("Enter first nummber: "));
-var number2 = parseInt(prompt("Enter second number: "));
 
-alert(add(number1, number2));
+
+function literToGallon(liter){
+	return liter / 3.785;
+}
+var liter = parseInt(prompt("Please enter liter(s): "))
+alert(liter+" is "+literToGallon(liter).toFixed(3)+ "gallon(s).");
